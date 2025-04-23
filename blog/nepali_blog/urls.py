5645457cbeb8f4d3249e8decd0a31359ea52d730
Sanjanshah <Sanjanshah102@gmail.com>
2025-04-23 16:51:50 +0545

    urls.py updated in app

---
+++ blog/nepali_blog/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import path 
+from . import views
+
+urlpatterns = [
+    path(" ", views. post_list, name="post_list"),
+    path("post/<slug:slug>/", views.post_detail, name="post_detail"),
+]
